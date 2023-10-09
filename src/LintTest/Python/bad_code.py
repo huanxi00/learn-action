f0e87f72f06db75694c6f2da296e401c96b7ae64
hangjinlei <hangjinlei@live.cn>
2023-10-09 15:30:16 +0000

    Add bad code test.

---
+++ src/LintTest/Python/bad_code.py
@@ -0,0 +1,31 @@
+from my_lib import Object
+
+import os
+
+from my_lib import Object3
+
+from my_lib import Object2
+
+import sys
+
+from third_party import lib15, lib1, lib2, lib3, lib4, lib5, lib6, lib7, lib8, lib9, lib10, lib11, lib12, lib13, lib14, lib15
+
+import sys
+
+from __future__ import absolute_import
+
+from third_party import lib3
+
+print("Hey")
+print("yo")
+
+j = [1,
+     2,
+     3
+]
+
+def very_important_function(template: str, *variables, file: os.PathLike, engine: str, header: bool = True, debug: bool = False):
+    """Applies `variables` to the `template` and writes to `file`."""
+    with open(file, 'w') as f:
+        ...
+
